1eb60354de7bc82a7e762c49e36a629251d7dd94
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-07-19 16:15:58 -0300

    update data

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,5 +1,6 @@
 export const experience = [
   {
+    id: 2,
     companyName: 'Linné Studio',
     jobTitle: {
       es: 'Desarrollador web freelance',
@@ -8,13 +9,14 @@ export const experience = [
     location: '',
     summary: {
       es: 'Cofundador y desarrollador. Creación de sitios web y aplicaciones móviles para clientes en equipo con una diseñadora. Me encargo de desarrollar tanto la funcionalidad como la UI de los proyectos en base a un diseño preexistente',
-      en: 'Co-founder and developer. Creation of websites and mobile applications for clients as a team with a designer. I am in charge of developing both the functionality and the UI of the projects based on a pre-existing design.',
+      en: 'Co-founder and developer. Creation of websites and mobile applications for clients as a team with a designer. I am in charge of developing both the functionality and the UI of the projects based on pre-existing designs.',
     },
     startDate: '02/2023',
     endDate: '',
-    image: '',
+    image: '/images/companies/linne.png',
   },
   {
+    id: 1,
     companyName: 'GBM Ingeniería y Ambiente',
     jobTitle: {
       es: 'Desarrollador web freelance',
@@ -22,17 +24,18 @@ export const experience = [
     },
     location: '',
     summary: {
-      es: 'Me desempeñé como desarrollador Front End React en un equipo conformado por otro desarrollador y un grupo de diseñadores UX/UI. Me encargué del desarrollo de la interfaz de usuario de un sitio web informativo y colaboré en la organización y toma de decisiones del equipo. Trabajé con React, Bootstrap y Styled-components.',
-      en: 'I worked as a Front End React developer as a team with another developer and a group of UX/UI designers. I was in charge of developing the user interface of an informative website and I cooperated in the organization and decision making of the team. I worked with React, Bootstrap and Styled-components.',
+      es: 'Me desempeñé como desarrollador Front End React en un equipo conformado por otro desarrollador y un grupo de diseñadores UX/UI. Me encargué del desarrollo de la interfaz de usuario de un sitio web informativo y colaboré en la organización y toma de decisiones del equipo.',
+      en: 'I worked as a Front End React developer as a team with another developer and a group of UX/UI designers. I was in charge of developing the user interface of an informative website and I cooperated in the organization and decision making of the team.',
     },
     startDate: '02/2023',
     endDate: '06/2023',
-    image: '',
+    image: '/images/companies/gbm.png',
   },
 ]
 
 export const education = [
   {
+    id: 11,
     certificationName: {
       es: 'Bootcamp de .NET',
       en: 'Microsoft .NET Bootcamp',
@@ -42,12 +45,17 @@ export const education = [
       en: '',
     },
     organization: 'Accenture',
-    certificateImage: '',
+    certificateImage: {
+      es: '/images/companies/accenture.png',
+      en: '/images/companies/accenture.png',
+    },
     startDate: '07/2023',
     endDate: '09/2023',
     technologies: [],
+    inProgress: true,
   },
   {
+    id: 10,
     certificationName: {
       es: 'Desarrollo de aplicaciones',
       en: 'Mobile Development',
@@ -57,24 +65,33 @@ export const education = [
       en: '',
     },
     organization: 'Coderhouse',
-    certificateImage: '',
+    certificateImage: {
+      es: '/images/companies/coderhouse.png',
+      en: '/images/companies/coderhouse.png',
+    },
     startDate: '06/2023',
     endDate: '08/2023',
     technologies: [],
+    inProgress: true,
   },
   {
+    id: 9,
     certificationName: { es: 'Full Stack Open', en: 'Full Stack Open' },
     description: {
       es: 'Project-based course that integrates modern Full Stack Development technologies. MERN, Redux, TypeScript, GraphQL, React Native, Docker and CI/CD.',
       en: 'Curso basado en proyectos que integra las tecnologías del desarrollo Full Stack moderno. MERN, Redux, TypeScript, GraphQL, React Native, Docker y CI/CD',
     },
     organization: 'University of Helsinki',
-    certificateImage: '',
+    certificateImage: {
+      es: '/images/certificates/fullstackopen.png',
+      en: '/images/certificates/fullstackopen.png',
+    },
     startDate: '03/2023',
     endDate: '07/2023',
     technologies: [],
   },
   {
+    id: 8,
     certificationName: {
       es: 'EF SET English Certificate 76/100 (C2 Proficient)',
       en: 'EF SET English Certificate 76/100 (C2 Proficient)',
@@ -83,13 +100,36 @@ export const education = [
       es: 'Test estandarizado de inglés. Conseguí un resultado de 76/100, que indica que mi nivel de inglés es C2 Proficient.',
       en: 'Standarized English test.',
     },
-    organization: 'EF Standard English Test (EF SET)',
-    certificateImage: '',
+    organization: 'EF Standard English Test',
+    certificateImage: {
+      es: '/images/certificates/efset.jpg',
+      en: '/images/certificates/efset.jpg',
+    },
     startDate: '',
     endDate: '02/2023',
     technologies: [],
   },
   {
+    id: 7,
+    certificationName: {
+      es: 'Desarrollo Full Stack',
+      en: 'Full Stack Development',
+    },
+    description: {
+      es: '',
+      en: '',
+    },
+    organization: 'Coderhouse',
+    certificateImage: {
+      es: '/images/certificates/coderhouse/carrera-full-stack.png',
+      en: '/images/certificates/coderhouse/carrera-full-stack_en.png',
+    },
+    startDate: '11/2022',
+    endDate: '05/2023',
+    technologies: [],
+  },
+  {
+    id: 6,
     certificationName: {
       es: 'Programación Backend',
       en: 'Backend Development',
@@ -99,12 +139,16 @@ export const education = [
       en: '',
     },
     organization: 'Coderhouse',
-    certificateImage: '',
+    certificateImage: {
+      es: '/images/certificates/coderhouse/backend.png',
+      en: '/images/certificates/coderhouse/backend_en.png',
+    },
     startDate: '11/2022',
     endDate: '05/2023',
     technologies: [],
   },
   {
+    id: 5,
     certificationName: {
       es: 'Algoritmos y Estructuras de Datos de JavaScript',
       en: 'JavaScript Algorithms and Data Structures',
@@ -114,36 +158,48 @@ export const education = [
       en: 'Curso práctico que incluye conceptos avanzados de JavaScript moderno (ES6+), programación orientada a objetos y programación funcional. El principal enfoque es el desarrollo de algoritmos para resolver problemas.',
     },
     organization: 'freeCodeCamp',
-    certificateImage: '',
+    certificateImage: {
+      es: '/images/certificates/freecodecamp-js.jpg',
+      en: '/images/certificates/freecodecamp-js.jpg',
+    },
     startDate: '01/2023',
     endDate: '02/2023',
     technologies: [],
   },
   {
+    id: 4,
     certificationName: { es: 'React', en: 'React' },
     description: {
       es: '',
       en: '',
     },
     organization: 'Coderhouse',
-    certificateImage: '',
+    certificateImage: {
+      es: '/images/certificates/coderhouse/react.png',
+      en: '/images/certificates/coderhouse/react_en.png',
+    },
     startDate: '09/2022',
     endDate: '10/2022',
     technologies: [],
   },
   {
+    id: 3,
     certificationName: { es: 'JavaScript', en: 'JavaScript' },
     description: {
       es: '',
       en: '',
     },
     organization: 'Coderhouse',
-    certificateImage: '',
+    certificateImage: {
+      es: '/images/certificates/coderhouse/javascript.png',
+      en: '/images/certificates/coderhouse/javascript_en.png',
+    },
     startDate: '07/2022',
     endDate: '08/2022',
     technologies: [],
   },
   {
+    id: 2,
     certificationName: {
       es: 'Conquering Responsive Layouts',
       en: 'Conquering Responsive Layouts',
@@ -153,19 +209,26 @@ export const education = [
       en: '',
     },
     organization: 'Kevin Powell',
-    certificateImage: '',
+    certificateImage: {
+      es: '/images/certificates/kevinpowell.jpg',
+      en: '/images/certificates/kevinpowell.jpg',
+    },
     startDate: '',
     endDate: '07/2022',
     technologies: [],
   },
   {
+    id: 1,
     certificationName: { es: 'Desarrollo Web', en: 'Web Development' },
     description: {
       es: '',
       en: '',
     },
     organization: 'Coderhouse',
-    certificateImage: '',
+    certificateImage: {
+      es: '/images/certificates/coderhouse/desarrollo-web.png',
+      en: '/images/certificates/coderhouse/desarrollo-web_en.png',
+    },
     startDate: '04/2022',
     endDate: '06/2022',
     technologies: [],
